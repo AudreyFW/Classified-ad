4c149db0cb6b405efff4d09fc4d9c7d8d86be080
Audrey <audrey.f.wirth@gmail.com>
2022-08-09 17:13:26 +0100

    add form

---
+++ jobAvailable.js
@@ -0,0 +1,30 @@
+export const availableJobs = [
+  {
+    id: 1,
+    title: "Analyst",
+    where: "London",
+    posted: 04 - 08 - 2022,
+    link: "link",
+  },
+  {
+    id: 2,
+    title: "Analyst",
+    where: "London",
+    posted: 04 - 08 - 2022,
+    link: "link",
+  },
+  {
+    id: 3,
+    title: "Analyst",
+    where: "London",
+    posted: 04 - 08 - 2022,
+    link: "link",
+  },
+  {
+    id: 4,
+    title: "Analyst",
+    where: "London",
+    posted: 04 - 08 - 2022,
+    link: "link",
+  },
+];
